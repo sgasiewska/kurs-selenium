f0c83ed634a2fcb8dff02b962e375a9930a946cc
sgasiewska <s.p.gasiewska@gmail.com>
2018-02-12 20:43:29 +0100

    Zadanie 3 Logowanie

---
+++ java-example/src/test/java/ru/stqa/training/selenium/LoginTest.java
@@ -0,0 +1,41 @@
+package ru.stqa.training.selenium;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.concurrent.TimeUnit;
+
+import static org.openqa.selenium.support.ui.ExpectedConditions.titleIs;
+
+public class LoginTest {
+    private WebDriver driver;
+    private WebDriverWait wait;
+
+    @Before
+    public void  strat(){
+        driver = new ChromeDriver();
+        //gdy nie znajdzie elementu to szuka gojeszcze przez 10 sekund
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        wait = new WebDriverWait(driver,10);
+    }
+
+    @Test
+    public void loginTest(){
+        driver.get("http://localhost/litecart/admin/login.php");
+        driver.findElement(By.name("username")).sendKeys("admin");
+        driver.findElement(By.name("password")).sendKeys("admin");
+        driver.findElement(By.name("login")).click();
+        wait.until(titleIs("My Store"));
+    }
+
+    @After
+    public void stop(){
+        driver.quit();
+        driver=null;
+    }
+}
