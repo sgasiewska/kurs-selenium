b6797fe38948c9cea6b74623d7ff0a0e21f996b7
sgasiewska <s.p.gasiewska@gmail.com>
2018-08-29 21:54:35 +0200

    Naprawa wyszukiwania google
    Trzeba było dodać
    Actions actions = new Actions(driver);
         actions.moveToElement(element).click().build().perform();

--- java-example/src/test/java/ru/stqa/training/selenium/MyFirstTest.java
+++ java-example/src/test/java/ru/stqa/training/selenium/MyFirstTest.java
@@ -3,24 +3,44 @@ package ru.stqa.training.selenium;
 
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import static java.util.concurrent.TimeUnit.SECONDS;
 import static org.openqa.selenium.support.ui.ExpectedConditions.titleIs;
 
 
 public class MyFirstTest extends TestBase {
 
 
-  @Test
-  public void myFirstTest(){
-    driver.get("http://www.google.com/");
-    driver.findElement(By.name("q")).sendKeys("webdriver");
-    driver.findElement(By.name("btnK")).click();
-    wait.until(titleIs("webdriver - Szukaj w Google"));
+ @Test
+  public void myFirstTest() {
+     driver.navigate().to("http://www.google.com");
+     driver.findElement(By.name("q")).sendKeys("webdriver");
+     //WebElement btnG=driver.findElement(By.xpath("//input[@value='Szukaj w Google']"));
+
+     WebElement element = driver.findElement(By.xpath("//input[@value='Szukaj w Google']"));
+     Actions actions = new Actions(driver);
+     actions.moveToElement(element).click().build().perform();
+     wait.until(titleIs("webdriver - Szukaj w Google"));
+     //btnG.click();
+ }
+
+ /*
+      driver.get("http://www.google.com/");
+      driver.findElement(By.name("q")).sendKeys("webdriver");
+      driver.findElement(By.name("btnK")).click();
+      wait.until(titleIs("webdriver - Szukaj w Google"));
   }
+  /*
     @Test
     public void mySecondTest(){
         driver.get("http://www.google.com/");
         driver.findElement(By.name("q")).sendKeys("webdriver");
-        driver.findElement(By.name("btnK")).click();
+        driver.findElement(By.cssSelector(".lsbt")).click();
         wait.until(titleIs("webdriver - Szukaj w Google"));
     }
+    */
 }
--- java-example/src/test/java/ru/stqa/training/selenium/TestBase.java
+++ java-example/src/test/java/ru/stqa/training/selenium/TestBase.java
@@ -2,7 +2,9 @@ package ru.stqa.training.selenium;
 
 import org.junit.After;
 import org.junit.Before;
+import org.openqa.selenium.By;
 import org.openqa.selenium.HasCapabilities;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
@@ -17,30 +19,62 @@ public class TestBase  {
 
     public static WebDriver driver;
     public static WebDriverWait wait;
+    public static ThreadLocal<WebDriver> tlDriver = new ThreadLocal<>();
+
+    //funkcja do spr obecnosci elementu wg okreslonego lokatora
+    public  boolean isElementPresent (By locator){
+        try{
+            driver.findElement(locator);
+            return  true;
+        } catch (NoSuchElementException ex){
+            return false;
+        }
+    }
+    // funkcja spr obecnosc elementow
+    public boolean areElementsPresent (By locator){
+        return driver.findElements(locator).size()>0;
+    }
+
+    @Before
+public void start() {
+        if (tlDriver.get() !=null){
+            driver = tlDriver.get();
+            wait= new WebDriverWait(driver, 10);
+            return;
+        }
+        driver = new ChromeDriver();
+        tlDriver.set(driver);
+        System.out.println(((HasCapabilities) driver).getCapabilities());
+    }
+}
+
+    /*
     @Before
     public void start(){
         //ta metoda będzie się wykonywać przed każdą met.testową i spr czy sterownik już jest
         if (driver!=null){
             return;
         }
-        /*
+
         DesiredCapabilities caps = new DesiredCapabilities();
         caps.setCapability(FirefoxDriver.MARIONETTE,false);
         driver = new FirefoxDriver(caps);
-        */
         FirefoxOptions options= new FirefoxOptions();
         options.setCapability(FirefoxDriver.MARIONETTE,false);
-        driver = new ChromeDriver(options);
+       //  driver = new ChromeDriver(options);
+        driver= new FirefoxDriver();
         System.out.println(((HasCapabilities) driver).getCapabilities());
-        wait=new WebDriverWait(driver,10);
+        wait=new WebDriverWait(driver,100);
 
         //zamykanie przeglądarki po wykonaniu wszystkich metod testowych
         Runtime.getRuntime().addShutdownHook(
                 new Thread(() -> {driver.quit(); driver=null;}));
     }
+
     @After
     public void stop(){
        // driver.quit();
       //  driver= null;
     }
 }
+*/
