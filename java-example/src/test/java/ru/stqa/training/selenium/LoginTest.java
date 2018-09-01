ee1f9336453fab415ff0d9d08eb7b9c7ab867c3f
sgasiewska <s.p.gasiewska@gmail.com>
2018-09-01 21:58:44 +0200

    5.5 Rozmiar i położenie elementu

--- java-example/src/test/java/ru/stqa/training/selenium/LoginTest.java
+++ java-example/src/test/java/ru/stqa/training/selenium/LoginTest.java
@@ -4,9 +4,7 @@ package ru.stqa.training.selenium;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -39,7 +37,20 @@ public class LoginTest {
 
         String color = komunikat.getCssValue("color");
 
+        //polozenie elementu- współzedne lewego górnego kąta elementu w pikselach w stosunku do lewego górnego kąta strony
+        Point location = komunikat.getLocation();
+        //szerokość i wysokosc elementu w pikselach
+        Dimension size = komunikat.getSize();
+        // "dwa w jednym" -- nowy sposób, którego jeszcze nie wszystkie sterowniki obsługują
+        //zwraca rozmiar i położenie elementu
+        Rectangle rect = komunikat.getRect();
+        // skroluje do elementu aby był widoczny-nie ma przykładu
+        //(Locatable element).getCoordinates().inView();
+
         System.out.println(color);
+        System.out.println(location);
+        System.out.println(size);
+        System.out.println(rect);
     }
 
     @After
