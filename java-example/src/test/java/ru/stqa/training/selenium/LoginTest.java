9acf60a0fc180127e21f16e1f68bc7c2aa86a81f
sgasiewska <s.p.gasiewska@gmail.com>
2018-09-01 17:42:07 +0200

    5.1 Otrzymywanie własciwosci elementów (getAttribute)

--- java-example/src/test/java/ru/stqa/training/selenium/LoginTest.java
+++ java-example/src/test/java/ru/stqa/training/selenium/LoginTest.java
@@ -18,6 +18,7 @@ public class LoginTest {
     private WebDriver driver;
     private WebDriverWait wait;
 
+
     @Before
     public void  strat(){
      //   driver = new ChromeDriver();
@@ -35,6 +36,9 @@ public class LoginTest {
         driver.findElement(By.name("password")).sendKeys("admin");
         driver.findElement(By.name("login")).click();
         wait.until(titleIs("My Store"));
+
+        String tekst=driver.findElement(By.xpath("(//th[@colspan='2'])")).getAttribute("textContent");
+        System.out.println(tekst);
     }
 
     @After
